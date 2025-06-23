19b052657f0cdb44b329783cc7f35be8d67ca66b
MuriloCod3 <161217664+MuriloCod3@users.noreply.github.com>
2025-06-23 12:34:15 -0300

    Add files via upload

---
+++ PwebNode/app.js
@@ -0,0 +1,23 @@
+let app = require('./app/config/server');   // carregando o modulo do servidor
+
+/*let rotaHome = require('./app/routes/home');    // definindo a rota
+rotaHome(app);                                  // executando
+
+let rotaAdicionarUsuario = require('./app/routes/adicionar_usuario');
+rotaAdicionarUsuario(app);
+
+let rotaHistoria = require('./app/routes/historia');
+rotaHistoria(app);
+
+let rotaProfessores = require('./app/routes/professores');
+rotaProfessores(app);
+
+let rotaCursos = require('./app/routes/cursos');
+rotaCursos(app);
+*/
+
+
+
+app.listen(3000, function(){
+    console.log("servidor com express foi carregado");
+});
